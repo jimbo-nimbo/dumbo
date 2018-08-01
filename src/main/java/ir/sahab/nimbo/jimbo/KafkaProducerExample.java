7f580d7a1fad7aabe1a6e8f8f8414435d949dda5
Kiarash Golezardi <golezardi@ce.sharif.edu>
2018-08-01 14:52:44 +0430

    Make producer example async

--- src/main/java/ir/sahab/nimbo/jimbo/KafkaProducerExample.java
+++ src/main/java/ir/sahab/nimbo/jimbo/KafkaProducerExample.java
@@ -6,6 +6,9 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class KafkaProducerExample {
     private final static String TOPIC = "TestTopic";
 
@@ -13,18 +16,26 @@ public class KafkaProducerExample {
         final Producer<Long, String> producer =
                 new KafkaProducer<>(KafkaConfig.getProducerProperties("KafkaProducerExample"));
         long time = System.currentTimeMillis();
+        final CountDownLatch countDownLatch = new CountDownLatch(sendMessageCount);
         try {
             for (long index = time; index < time + sendMessageCount; index++) {
                 final ProducerRecord<Long, String> record =
                         new ProducerRecord<>(TOPIC, index,
                                 "Hello Mom " + index);
-                RecordMetadata metadata = producer.send(record).get();
-                long elapsedTime = System.currentTimeMillis() - time;
-                System.out.printf("sent record(key=%s value=%s) " +
-                                "meta(partition=%d, offset=%d) time=%d\n",
-                        record.key(), record.value(), metadata.partition(),
-                        metadata.offset(), elapsedTime);
+                producer.send(record, (metadata, exception) -> {
+                    long elapsedTime = System.currentTimeMillis() - time;
+                    if (metadata != null) {
+                        System.out.printf("sent record(key=%s value=%s) " +
+                                        "meta(partition=%d, offset=%d) time=%d\n",
+                                record.key(), record.value(), metadata.partition(),
+                                metadata.offset(), elapsedTime);
+                    } else {
+                        exception.printStackTrace();
+                    }
+                    countDownLatch.countDown();
+                });
             }
+            countDownLatch.await(25, TimeUnit.SECONDS);
         } finally {
             producer.flush();
             producer.close();
