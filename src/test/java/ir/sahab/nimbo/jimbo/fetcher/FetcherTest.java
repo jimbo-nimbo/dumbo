58a5ec8cfd79d69b7320f4e88250ebc47a2b6600
Kiarash Golezardi <golezardi@ce.sharif.edu>
2018-08-09 12:06:16 +0430

    Split checking and adding to LRU

--- src/test/java/ir/sahab/nimbo/jimbo/fetcher/FetcherTest.java
+++ src/test/java/ir/sahab/nimbo/jimbo/fetcher/FetcherTest.java
@@ -5,14 +5,4 @@ import org.junit.Test;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class FetcherTest {
-
-    @Test
-    public void getUrlBody() {
-        FetcherFactory fetcherFactory = new FetcherFactory(new ArrayBlockingQueue(50));
-        fetcherFactory.newFetcher().run();
-    }
-
-    @Test
-    public void run() {
-    }
 }
