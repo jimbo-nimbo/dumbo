1630bad65db63a77c3b0a5fc18ed7d7484ec7ae5
nimac <michael12sco@yahoo.com>
2018-08-14 09:33:53 +0430

    -- testing MapR

--- twitter-spark/src/main/java/ir/sahab/nimbo/jimbo/sparkstream/SparkStream.java
+++ twitter-spark/src/main/java/ir/sahab/nimbo/jimbo/sparkstream/SparkStream.java
@@ -82,8 +82,9 @@ public class SparkStream implements Serializable {
                 s -> new Tuple2<>(s, 1));
         JavaPairDStream<String, Integer> counting = numbering.reduceByKey((Function2<Integer, Integer, Integer>)
                 (integer, integer2) -> integer + integer2);
-        counting.saveAsHadoopFiles("hdfs://localhost:9000/spark/","txt", Text.class,
-                IntWritable.class, TextOutputFormat.class);
+        counting.dstream().saveAsTextFiles("hdfs://46.105.100.63:9000/spark/","txt");
+        //counting.saveAsHadoopFiles("hdfs://46.105.100.63:9000/spark/","txt", Text.class,
+                //IntWritable.class, TextOutputFormat.class);
         //counting.dstream().saveAsTextFiles("nimac",".rawFile");
 //        counting.foreachRDD(new VoidFunction<JavaPairRDD<String, Integer>>() {
 //            @Override
