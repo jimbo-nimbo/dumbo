2c9ce5cd2fa9aa46b2ea8a99a32a1394a5931927
Kiarash Golezardi <golezardi@ce.sharif.edu>
2018-09-05 16:40:29 +0430

    Initiate low level REST API for Keywords

--- keywords/src/main/java/ir/sahab/nimbo/jimbo/Main.java
+++ keywords/src/main/java/ir/sahab/nimbo/jimbo/Main.java
@@ -1,6 +1,16 @@
 package ir.sahab.nimbo.jimbo;
 
+import ir.sahab.nimbo.jimbo.elasticsearch.ElasticClientBuilder;
+import ir.sahab.nimbo.jimbo.elasticsearch.ElasticConfig;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilder;
@@ -9,17 +19,24 @@ import org.elasticsearch.index.reindex.ReindexAction;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 
 public class Main {
-    public static void main2(String[] args) throws UnknownHostException {
+    public static void reindex(String[] args) throws UnknownHostException {
 
-        TransportClient client = ElasticClientBuilder.build();
+        TransportClient client = ElasticClientBuilder.buildTransport();
         QueryBuilder qb = matchAllQuery();
         BulkByScrollResponse response = ReindexAction.INSTANCE.newRequestBuilder(client)
                 .source(ElasticConfig.INDEX_NAME)
@@ -27,8 +44,8 @@ public class Main {
                 .get();
     }
 
-    public static void main(String[] args) throws UnknownHostException {
-        TransportClient client = ElasticClientBuilder.build();
+    public static void transportScroll(String[] args) throws UnknownHostException {
+        TransportClient client = ElasticClientBuilder.buildTransport();
         QueryBuilder qb = matchAllQuery();
 
         SearchResponse scrollResp = client.prepareSearch(ElasticConfig.INDEX_NAME)
@@ -40,16 +57,12 @@ public class Main {
         int total = 0;
 
         do {
+            List<String> docIds = new ArrayList<>();
             for (SearchHit hit : scrollResp.getHits().getHits()) {
-                System.out.println("\"" + hit.getId() + "\",");
-//                Map<String, Object> source = hit.getSourceAsMap();
-//                String url = (String) source.get("url");
-//                String content = (String) source.get("content");
-//                String title = (String) source.get("title");
-//                String description = (String) source.get("description");
-                total++;
+                docIds.add(hit.getId());
+
             }
-             System.out.println("Next... " + total);
+            System.out.println("Next... " + total);
 
             scrollResp = client.prepareSearchScroll(scrollResp.getScrollId())
                     .setScroll(new TimeValue(180, TimeUnit.SECONDS))
@@ -59,4 +72,48 @@ public class Main {
 
         client.close();
     }
+
+    public static void main(String[] args) throws IOException {
+        RestClient restClient = ElasticClientBuilder.buildRest();
+
+//        Request request = new Request("POST", "/" + ElasticConfig.INDEX_NAME + "/_search");
+//        request.addParameter("scroll", "1m");
+//        Response response = restClient.performRequest(request);
+
+        //System.out.println(EntityUtils.toString(response.getEntity()));
+//        JSONObject jsonObject = new JSONObject(EntityUtils.toString(response.getEntity()));
+        //System.out.println(jsonObject.get("tagline"));
+
+        restClient.close();
+    }
+
+    /*public void getTermVector(String id) throws IOException {
+        Map<String, String> params = Collections.emptyMap();
+        String jsonString =
+                "{\n" +
+                        "  \"fields\" : [\"content\"],\n" +
+                        "  \"offsets\" : true,\n" +
+                        "  \"payloads\" : true,\n" +
+                        "  \"positions\" : true,\n" +
+                        "  \"term_statistics\" : true,\n" +
+                        "  \"field_statistics\" : true\n" +
+                        "}";
+        HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
+        Response response =
+                restClient.performRequest("GET", "/" + index + "/_doc/" + id + "/_termvectors", params, entity);
+        // System.out.println(response.toString());
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+        }
+        System.out.println(out.toString());
+        JSONObject jsonObject = new JSONObject(out.toString());
+        JSONObject jsonArray = jsonObject.getJSONObject("term_vectors").getJSONObject("content").getJSONObject("terms");
+        for (String key : jsonArray.keySet()) {
+            System.out.println(key + "=" + jsonArray.get(key)); // to get the value
+        }
+    }*/
 }
